a258ec1e8eeb81eaebe9c98265a54fafe63e1f96
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-05 13:34:22 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 
 int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
-    printf("Введите своё имя:\n");
-    str = gets(str);
-    printf("Привет, %s", str);
+    printf("Введите своё имя:\n");   // из main
+    str = gets(str);                 // из main
+    puts("Привет");                  // из rebasing
+    puts(str);                       // из rebasing
     return 0;
 }
 
